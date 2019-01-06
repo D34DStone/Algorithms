89cbd92f482f20edec5fb5b3d76c2156f211c490
D34DStone <deadstonepro@gmail.com>
2019-01-06 17:36:13 +0200

    refactored a little bit

--- segment_tree/defaultSTNodes.hpp
+++ segment_tree/defaultSTNodes.hpp
@@ -1,60 +1,56 @@
+#ifndef DEFAULT_ST_NODES_H_
+#define DEFAULT_ST_NODES_H_
+
 #include "algorithm"
 
 namespace GAlgo 
 {
-    class IntSum
+    struct IntSum
     {
-    public:
-        int val = 0;
+        int val;
 
         static IntSum merge(const IntSum &a, const IntSum &b)
         {
-            IntSum newValue;
-            newValue.val = a.val + b.val;
+            IntSum newValue; newValue.val = a.val + b.val;
             return newValue;
         }
 
         static IntSum fromInt(int a)
         {
-            IntSum newValue;
-            newValue.val = a;
+            IntSum newValue; newValue.val = a;
             return newValue;
         }
 
         static IntSum neutral()
         {
-            IntSum newValue;
-            newValue.val = 0;
+            IntSum newValue; newValue.val = 0;
             return newValue;
         }
     };
 
-    class IntMin
+    struct IntMin
     {
-    public:
         static const int INF = 1e9 - 1;
-
-        int val = INF;
+        int val;
 
         static IntMin merge(const IntMin &a, const IntMin &b)
         {
-            IntMin newValue;
-            newValue.val = std::min(a.val, b.val);
+            IntMin newValue; newValue.val = std::min(a.val, b.val);
             return newValue;
         }
 
         static IntMin fromInt(int a)
         {
-            IntMin newValue;
-            newValue.val = a;
+            IntMin newValue; newValue.val = a;
             return newValue;
         }
 
         static IntMin neutral()
         {
-            IntMin newValue;
-            newValue.val = INF;
+            IntMin newValue; newValue.val = INF;
             return newValue;
         }
     };
 };
+
+#endif//DEFAULT_ST_NODES_H_
